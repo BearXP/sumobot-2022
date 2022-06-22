b2f77e199f20b4fbc5924f59a76c8c83de140ee9
BearXP <president.evans@gmail.com>
2022-06-22 22:11:59 +1000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,47 @@
-/**
- * https://github.com/mworkfun/pxt-k-bit.git
- */
+function ReadSensors () {
+    if (input.buttonIsPressed(Button.AB)) {
+        NewMove = 8
+    } else if (input.buttonIsPressed(Button.A)) {
+        NewMove = 7
+    } else if (input.buttonIsPressed(Button.B)) {
+        NewMove = 9
+    }
+}
+// https://github.com/mworkfun/pxt-k-bit.git
+function Move () {
+    if (LastMove == NewMove) {
+        LastMove = NewMove
+        if (NewMove == 7) {
+            basic.showLeds(`
+                # # # # .
+                # # . . .
+                # . # . .
+                # . . # .
+                . . . . #
+                `)
+        } else if (NewMove == 8) {
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # . # . #
+                . . # . .
+                . . # . .
+                `)
+        } else if (NewMove == 9) {
+            basic.showLeds(`
+                . # # # #
+                . . . # #
+                . . # . #
+                . # . . #
+                # . . . .
+                `)
+        }
+    }
+}
+let NewMove = 0
+let LastMove = 0
+LastMove = 5
+NewMove = 5
 basic.showLeds(`
     . # . # .
     # # # # #
@@ -9,5 +50,6 @@ basic.showLeds(`
     . . # . .
     `)
 basic.forever(function () {
-	
+    ReadSensors()
+    Move()
 })
