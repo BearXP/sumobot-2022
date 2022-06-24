83b105ac94695fe81838b99075360d00a0e1f079
BearXP <president.evans@gmail.com>
2022-06-24 17:06:07 +1000

    Sensors work turns the wrong way. I want to test what's happening..

--- main.ts
+++ main.ts
@@ -2,32 +2,52 @@ function DetectedSensorLeft () {
     SensorRawLeft = k_Bit.obstacle(MotorObs.LeftSide)
     return !(SensorRawLeft)
 }
-function ReadSensors () {
-    if (DetectedFrontClose()) {
-        NewMove = 8
-    } else if (input.buttonIsPressed(Button.A)) {
-        NewMove = 7
-    } else if (input.buttonIsPressed(Button.B)) {
-        NewMove = 9
-    }
-}
-function DetectedSensorRight () {
+function DetectedSensorRight() {
     SensorRawRight = k_Bit.obstacle(MotorObs.RightSide)
     return !(SensorRawRight)
 }
+function ReadSensors () {
+    if (DetectedFrontClose()) {
+        NewMove = 8
+    } else if (DetectedSensorLeft()) {
+        NewMove = 7
+    } else if (DetectedSensorRight()) {
+        NewMove = 9
+    } else if (DetectedEgde()) {
+        NewMove = 2
+    } else {
+        NewMove = 5
+    }
+}
+function ReadRemote () {
+    RemoteButtonPressed = irRemote.returnIrButton()
+    if (RemoteButtonPressed != 0) {
+        SerialDebug("    > REMOTE " + RemoteButtonPressed)
+    }
+    if (irRemote.irButton(IrButton.Up) == 1) {
+        UltrasonicDistance = 8
+    }
+}
+function DetectedEgde () {
+    SensorEdge = k_Bit.LineTracking()
+    return !(SensorEdge)
+}
 // https://github.com/mworkfun/pxt-k-bit.git
 function Move () {
-    if (LastMove == NewMove) {
+    if (LastMove != NewMove) {
         LastMove = NewMove
         if (NewMove == 7) {
+            k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, MotorSpeed)
+            k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, 0)
             basic.showLeds(`
-                # # # # .
-                # # . . .
-                # . # . .
-                # . . # .
-                . . . . #
+                . # # # #
+                . . . # #
+                . . # . #
+                . # . . #
+                # . . . .
                 `)
         } else if (NewMove == 8) {
+            k_Bit.run(DIR.RunForward, MotorSpeed)
             basic.showLeds(`
                 . . # . .
                 . # # # .
@@ -36,20 +56,29 @@ function Move () {
                 . . # . .
                 `)
         } else if (NewMove == 9) {
+            k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, 0)
+            k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, MotorSpeed)
             basic.showLeds(`
-                . # # # #
-                . . . # #
-                . . # . #
-                . # . . #
-                # . . . .
+                # # # # .
+                # # . . .
+                # . # . .
+                # . . # .
+                . . . . #
+                `)
+        } else if (NewMove == 2) {
+            k_Bit.run(DIR.RunBack, MotorSpeed)
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # . # . #
+                . # # # .
+                . . # . .
                 `)
         }
     }
 }
 function DetectedFrontClose () {
-    if (input.buttonIsPressed(Button.AB)) {
-        return 1
-    } else if (UltrasonicDistance < 30) {
+    if (UltrasonicDistance < 30 && UltrasonicDistance > 1) {
         return 1
     } else if (DetectedSensorLeft() && DetectedSensorRight()) {
         return 1
@@ -57,11 +86,19 @@ function DetectedFrontClose () {
         return 0
     }
 }
-let UltrasonicDistance = 0
+function SerialDebug (text: string) {
+    serial.writeLine(text)
+}
+let SensorEdge = 0
 let SensorRawRight = 0
+let UltrasonicDistance = 0
+let RemoteButtonPressed = 0
 let SensorRawLeft = 0
 let NewMove = 0
 let LastMove = 0
+let MotorSpeed = 0
+MotorSpeed = 30
+irRemote.connectInfrared(DigitalPin.P1)
 LastMove = 5
 NewMove = 5
 basic.showLeds(`
@@ -72,16 +109,14 @@ basic.showLeds(`
     . . # . .
     `)
 basic.forever(function () {
+    ReadRemote()
     ReadSensors()
     Move()
 })
 control.inBackground(function () {
     while (true) {
-        UltrasonicDistance = sonar.ping(
-        DigitalPin.P14,
-        DigitalPin.P15,
-        PingUnit.Centimeters
-        )
+        UltrasonicDistance = k_Bit.ultra()
+        // SerialDebug("UltrasonicDistance" + UltrasonicDistance)
         basic.pause(200)
     }
 })
